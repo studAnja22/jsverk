8bf098e416153ce57fcbb1f076e4ce81a8e34f22
Oscar Wystråle (osay21) <osay21@student.bth.se>
2023-09-21 15:31:09 +0200

    setup leaflet map

--- svelte/src/lib/Leaflet.svelte
+++ svelte/src/lib/Leaflet.svelte
@@ -1,7 +1,51 @@
 <script lang="ts">
-    let mapElement: HTMLDivElement;
+	import { onMount, onDestroy, setContext, createEventDispatcher } from 'svelte';
+	import L from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+
+	export let bounds: L.LatLngBoundsExpression | undefined = undefined;
+	export let view: L.LatLngExpression | undefined = undefined;
+	export let zoom: number | undefined = undefined;
+
+	const dispatch = createEventDispatcher();
+
+	let map: L.Map | undefined;
+	let mapElement: HTMLElement;
+
+	onMount(() => {
+		if (!bounds && (!view || !zoom)) {
+			throw new Error('Must set either bounds, or view and zoom.');
+		}
+
+		map = L.map(mapElement)
+			// example to expose map events to parent components:
+			.on('zoom', (e) => dispatch('zoom', e));
+
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+			attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,&copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`
+		}).addTo(map);
+	});
+
+	onDestroy(() => {
+		map?.remove();
+		map = undefined;
+	});
+
+	setContext('map', {
+		getMap: () => map
+	});
+
+	$: if (map) {
+		if (bounds) {
+			map.fitBounds(bounds);
+		} else if (view && zoom) {
+			map.setView(view, zoom);
+		}
+	}
 </script>
 
-<div style="width: 100% heigh: 100%" bind:this={mapElement}>
-    <slot />
+<div class="w-full h-full" bind:this={mapElement}>
+	{#if map}
+		<slot />
+	{/if}
 </div>
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-    let mapElement: HTMLDivElement;
+	import '../app.css';
+	import type { LatLngExpression } from 'leaflet';
+	import Leaflet from '$lib/Leaflet.svelte';
+
+    const initialView: LatLngExpression = [62.173276, 14.942265];
 </script>
 
-<div class="w-full h-full" bind:this={mapElement}>
-    <slot />
+<div class="w-full h-screen">
+	<Leaflet view={initialView} zoom={5} />
 </div>

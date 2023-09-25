8103805d7488b0dc520c6fb8e0720ba324eafe70
Oscar Wystråle (osay21) <osay21@student.bth.se>
2023-09-25 10:32:20 +0200

    Leaflet markers setup

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -2,10 +2,21 @@
 	import '../app.css';
 	import type { LatLngExpression } from 'leaflet';
 	import Leaflet from '$lib/Leaflet.svelte';
+	import Marker from '$lib/Marker.svelte';
+	import Popup from '$lib/Popup.svelte';
 
     const initialView: LatLngExpression = [62.173276, 14.942265];
+	const markerLocations: Array<LatLngExpression> = [
+	[62.173276, 14.942265]
+	];
 </script>
 
 <div class="w-full h-screen">
-	<Leaflet view={initialView} zoom={5} />
+	<Leaflet view={initialView} zoom={5}>
+		{#each markerLocations as latLng}
+			<Marker {latLng} width={40} height={40}>
+				<Popup>Like & Subscribe!</Popup>
+			</Marker>
+		{/each}
+	</Leaflet>
 </div>
